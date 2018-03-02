d15f14d7c6060186015a7ca778dcfeca1174c40f
Michael Hrizuk <mhrizuk@michaels-mbp.lan>
2018-03-02 13:39:21 -0600

    second initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+    render() {
+        return (
+        <div className="App">
+            <h1>hi, im a react app</h1>
+            <p>Meow Mix is truly trash can food</p>
+        </div>
+        );
+    }
+}
+
+export default App;
